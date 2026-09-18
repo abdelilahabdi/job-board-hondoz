1e73c0c45e7b10da7039f461ff49dad336b32eba
abdelilah abdi <abdelilahabdi6633@gmail.com>
2026-09-18 17:54:54 +0100

    feat(apply): implement apply feature workflow and update track page sections

--- js/offers-submitting.js
+++ js/offers-submitting.js
@@ -1,23 +1,34 @@
-import {getData} from "./utils/data.js"
-import { renderOffers } from "./utils/render.js"
-import { getFavorites } from "./utils/storage.js"
+import { getData } from "./utils/data.js";
+import { renderOffers } from "./utils/render.js";
+import { getFavorites, getApplications } from "./utils/storage.js";
+
+const favoritesGrid = document.querySelector(".favorites-grid");
+const applicationsGrid = document.querySelector(".applications-grid");
+
+const allOffers = await getData();
 
 
-const gridContainer = document.querySelector(".grid");
+const favoriteIds = new Set(getFavorites().map(id => String(id)));
+const favoriteOffers = allOffers.filter(offer => favoriteIds.has(String(offer.id)));
 
-
-const allOffers = await getData() ;
-const favoriteIds = getFavorites();
-
-
-const favoriteOffers = allOffers.filter(offer => favoriteIds.includes(String(offer.id)));
-
-gridContainer.innerHTML = "" ;
-if (favoriteOffers.length === 0) {
-    gridContainer.innerHTML = "<p>no favorite offers saved yet. </p>";
-
-} else {
-    renderOffers(gridContainer, favoriteOffers);
+if (favoritesGrid) {
+    favoritesGrid.innerHTML = "";
+    if (favoriteOffers.length === 0) {
+        favoritesGrid.innerHTML = "<p>No favorite offers saved yet.</p>";
+    } else {
+        renderOffers(favoritesGrid, favoriteOffers);
+    }
 }
 
 
+const appliedIds = new Set(getApplications().map(id => String(id)));
+const appliedOffers = allOffers.filter(offer => appliedIds.has(String(offer.id)));
+
+if (applicationsGrid) {
+    applicationsGrid.innerHTML = "";
+    if (appliedOffers.length === 0) {
+        applicationsGrid.innerHTML = "<p>No applications submitted yet.</p>";
+    } else {
+        renderOffers(applicationsGrid, appliedOffers);
+    }
+}
